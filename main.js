f0e68d714e6f804d109077bb1eaed94519be372d
Alazar-dev <mealazartadesse@gmail.com>
2020-10-21 14:52:54 +0300

    Rendering

--- main.js
+++ main.js
@@ -4,10 +4,15 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
                 {id: 1122, color: 'green'},
                 {id: 1212, color: 'blue'}
+            ],
+            shipping: [
+                {Ethiopia: '$50'}, 
+                {America: '$35'},
+                {North_Korea: 'No shipping there!'}
             ]
         }
     }
